f6216dd27292635fc00c1db005f7416c390bc8e3
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-06 16:56:33 -0500

    first commit

---
+++ lua/nihilc/configs/keymaps.lua
@@ -0,0 +1,51 @@
+function Map(keymaps)
+    local default_opts = {
+        silent = true,
+        remap = true
+    }
+    for mode, mode_keymaps in pairs(keymaps) do
+        for lhs, args in pairs(mode_keymaps) do
+            local rhs = args[1]
+            local opts = args.opts or default_opts
+            opts.desc = args[2]
+
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
+    end
+end
+
+local default_keymaps = {
+    n = {
+        -- Basic
+        ["<c-s>"] = { "<cmd>w<cr>", "Save" },
+        ["<esc>"] = { "<cmd>noh<cr>", "Clear highlights" },
+        -- Windows
+        ["<m-k>"] = { "<c-w>k", "Move to window up" },
+        ["<m-j>"] = { "<c-w>j", "Move to window down" },
+        ["<m-h>"] = { "<c-w>h", "Move to window left" },
+        ["<m-l>"] = { "<c-w>l", "Move to window right" },
+        -- Buffers
+        ["<tab>"] = { "<cmd>bn<cr>", "Buffer next" },
+        ["<s-tab>"] = { "<cmd>bp<cr>", "Buffer prev" },
+        ["<leader>bn"] = { "<cmd>new<cr>", "Buffer new" },
+        ["<leader>bx"] = { "<cmd>bdelete<cr>", "Buffer close" },
+    },
+    i = {
+        -- Movement in insert mode
+        ["<m-k>"] = { "<up>", "Move up" },
+        ["<m-j>"] = { "<down>", "Move down" },
+        ["<m-h>"] = { "<left>", "Move left" },
+        ["<m-l>"] = { "<right>", "Move right" },
+    },
+    v = {},
+    t = {},
+    c = {
+        -- Movement in command mode
+        ["<m-k>"] = { "<up>", "Prev command", opts = { silent = false } },
+        ["<m-j>"] = { "<down>", "Next command", opts = { silent = false } },
+        ["<m-h>"] = { "<left>", "Move left", opts = { silent = false } },
+        ["<m-l>"] = { "<right>", "Move right", opts = { silent = false } },
+    },
+}
+
+Map(default_keymaps)
---
+++ lua/nihilc/configs/options.lua
@@ -0,0 +1,60 @@
+local o = vim.opt
+local g = vim.g
+
+---- Globals
+
+g.mapleader         = " "
+g.maplocalleader    = " "
+
+g.python3_host_prog = "/usr/bin/python3"
+
+-- Disable providers
+g.loaded_node_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+
+---- Options
+
+-- UI
+o.laststatus        = 3     -- always show status line and only one
+o.ruler             = true  -- show line and column number of cursor
+o.cursorline        = true  -- highlights cursor line
+o.showmode          = true  -- show mode
+o.signcolumn        = "yes" -- always show sign column
+o.termguicolors     = true  -- true color support
+
+-- Visual
+o.number            = true  -- line number
+o.numberwidth       = 2     -- line number width
+o.relativenumber    = true  -- line relative number
+o.scrolloff         = 4     -- lines of context
+o.sidescrolloff     = 8     -- lines of context
+o.wrap              = false -- line wrap
+o.splitright        = true  -- new windows below current
+o.splitbelow        = true  -- new windows right of current
+o.pumheight         = 10    -- pop-up height
+o.pumblend          = 10    -- pop-up transparency 0-100
+
+-- Indentation
+o.smartindent       = true  -- smart indent on new line
+o.expandtab         = true  -- use spaces 
+o.shiftround        = true  -- round indent
+o.shiftwidth        = 4     -- indent size
+o.tabstop           = 4     -- indent size
+o.softtabstop       = 4     -- indent size
+
+-- Search
+o.ignorecase        = true  -- ignore case in search
+o.smartcase         = true  -- Override ignore case if search has upper case
+
+-- Files
+o.undofile          = true  -- Save undo file
+o.undolevels        = 10000 -- number of changes to undo
+
+-- Clipboard
+o.clipboard         = "unnamedplus"
+
+-- Spell
+o.spell             = true      -- enable spell
+o.spelloptions      = "camel"   -- fix camelCase spell
+o.spelllang         = {"en_us", "es"}
