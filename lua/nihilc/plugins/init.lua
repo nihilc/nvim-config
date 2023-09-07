00a61fafef0d3b5f2a3f2c7528558ee8f788beac
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-07 07:17:10 -0500

    move theme to new file

--- lua/nihilc/plugins/init.lua
+++ lua/nihilc/plugins/init.lua
@@ -1,15 +1,3 @@
 return {
-    -- Theme
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = true,
-            })
-            vim.cmd("colorscheme catppuccin")
-        end,
-    },
+    -- Put here plugins that not need config
 }
