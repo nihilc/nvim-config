6b60da289bcb67d216ec68bbd038d5b0c457e5ae
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-11 01:42:37 -0500

    update telescope maps and desc

--- lua/nihilc/plugins/telescope.lua
+++ lua/nihilc/plugins/telescope.lua
@@ -2,14 +2,18 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.2",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons"
+        },
         keys = {
-            {"<leader>ff", function() require('telescope.builtin').find_files()  end, desc = "find file"},
-            {"<leader>fw", function() require('telescope.builtin').live_grep()   end, desc = "find word"},
-            {"<leader>fb", function() require('telescope.builtin').buffers()     end, desc = "find buffer"},
-            {"<leader>th", function() require('telescope.builtin').help_tags()   end, desc = "show help tags"},
-            {"<leader>tk", function() require('telescope.builtin').keymaps()     end, desc = "show keymaps"},
-            {"<leader>tc", function() require('telescope.builtin').colorscheme() end, desc = "change colorscheme"},
+            {"<leader>ff", function() require('telescope.builtin').find_files() end, desc = "(F)ind (F)ile"},
+            {"<leader>fw", function() require('telescope.builtin').live_grep()  end, desc = "(F)ind (W)ord"},
+            {"<leader>fb", function() require('telescope.builtin').buffers()    end, desc = "(F)ind (B)uffer"},
+            {"<leader>ht", function() require('telescope.builtin').help_tags()  end, desc = "(H)elp (T)ags"},
+            {"<leader>hk", function() require('telescope.builtin').keymaps()    end, desc = "(H)elp (K)eymaps"},
+            {"<leader>tt", function() require('telescope.builtin').builtin()    end, desc = "(T)oggle (T)Telescope"},
         },
         -- TODO config interface
         -- opts = {}
