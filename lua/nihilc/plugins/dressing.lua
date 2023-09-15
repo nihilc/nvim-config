1b813ee53c1668b4b6c914293a6d1365d813b62b
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-14 20:01:03 -0500

    add dressing plugin

---
+++ lua/nihilc/plugins/dressing.lua
@@ -0,0 +1,16 @@
+return {
+    'stevearc/dressing.nvim',
+    event = { "VeryLazy" },
+    opts = {
+        input = {
+            win_options = {
+                winblend = 0,
+            },
+        },
+        select = {
+            win_options = {
+                winblend = 0,
+            },
+        },
+    },
+}
