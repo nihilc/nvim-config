822d3ca09f4f2e9684a2de01b04ccd34dfbc9ee7
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-15 23:16:01 -0500

    add lsp config

--- lua/nihilc/init.lua
+++ lua/nihilc/init.lua
@@ -7,28 +7,28 @@ require("nihilc.configs.keymaps")
 -- Setup plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("nihilc.plugins",{
-    install = {
-        colorscheme = { "catppuccin", "habamax" },
-    },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {
-        notify = false,
-    },
+require("lazy").setup({ { import = "nihilc.plugins" }, { import = "nihilc.plugins.lsp" } }, {
+	install = {
+		colorscheme = { "catppuccin", "habamax" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
 
 vim.cmd("colorscheme catppuccin")
---
+++ lua/nihilc/plugins/lsp/lspconfig.lua
@@ -0,0 +1,88 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		-- import lspconfig plugin
+		local lspconfig = require("lspconfig")
+
+		-- import cmp-nvim-lsp plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+		local opts = { noremap = true, silent = true }
+		local on_attach = function(client, bufnr)
+			opts.buffer = bufnr
+			Map({
+				n = {
+					K = { vim.lsp.buf.hover, "LSP hover", opts },
+					gD = { vim.lsp.buf.declaration, "Go to declaration", opts },
+					gd = { "<cmd>Telescope lsp_definitions<cr>", "Go LSP definitions", opts },
+					gR = { "<cmd>Telescope lsp_references<cr>", "Go LSP references", opts },
+					gi = { "<cmd>Telescope lsp_implementations<cr>", "Go LSP implementations", opts },
+					gt = { "<cmd>Telescope lsp_type_definitions<cr>", "Go LSP type definitions", opts },
+					["[d"] = { vim.diagnostic.goto_prev, "Go to previous diagnostic", opts },
+					["]d"] = { vim.diagnostic.goto_next, "Go to next diagnostic", opts },
+					["<leader>la"] = { vim.lsp.buf.code_action, "LSP code actions", opts },
+					["<leader>lr"] = { vim.lsp.buf.rename, "LSP rename", opts },
+					["<leader>lf"] = { vim.lsp.buf.format, "LSP format", opts },
+					["<leader>ld"] = { vim.diagnostic.open_float, "Show line diagnostics", opts },
+					["<leader>lD"] = { "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics", opts },
+				},
+			})
+		end
+
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		local servers = {
+			"html",
+			"cssls",
+			"tsserver",
+			"pyright",
+			"clangd",
+		}
+		for _, server in ipairs(servers) do
+			lspconfig[server].setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end
+
+		-- configure emmet language server
+		lspconfig["emmet_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+		})
+
+		-- configure lua server (with special settings)
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = { -- custom settings for lua
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						-- make language server aware of runtime files
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
+	end,
+}
