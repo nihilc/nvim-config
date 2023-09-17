d17ba6374893a6220719cada8ebe6d74124ce229
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-16 21:08:41 -0500

    add and organice formatters and linter

--- lua/nihilc/plugins/lsp/mason.lua
+++ lua/nihilc/plugins/lsp/mason.lua
@@ -23,13 +23,17 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"html",
-				"emmet_ls",
-				"cssls",
-				"tsserver",
+				-- Web
+				"html", -- html
+				"emmet_ls", -- html
+				"cssls", -- css
+				"tsserver", -- js/ts
+				"intelephense", -- php
 
-				"lua_ls",
-				"pyright",
+				"lua_ls", -- lua
+				"bashls", -- bash
+				"clangd", -- c/cpp
+				"pyright", -- python
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
@@ -39,8 +43,13 @@ return {
 			-- list of formatters & linters for mason to install
 			ensure_installed = {
 				"stylua", -- lua formatter
+				"shellcheck", -- bash linter
+				"beautysh", -- bash linter
 				"cland-format", -- c/cpp formatter
-				"prettier", -- ts/js formatter
+				"phpcs", -- php linter
+				"phpcbf", -- php formater
+				"prettier", -- js/ts formatter
+				"eslint_d", -- js/ts linter
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true,
--- lua/nihilc/plugins/lsp/null-ls.lua
+++ lua/nihilc/plugins/lsp/null-ls.lua
@@ -2,19 +2,48 @@ return {
 	"jose-elias-alvarez/null-ls.nvim",
 	event = "VeryLazy",
 	config = function()
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		local null_ls = require("null-ls")
 		local null_ls_utils = require("null-ls.utils")
+
 		local formatting = null_ls.builtins.formatting
 		local diagnostics = null_ls.builtins.diagnostics
 
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		local sources = {
+			-- LUA
+			formatting.stylua,
+
+			-- Bash
+			diagnostics.shellcheck,
+			formatting.beautysh,
+
+			-- C / CPP
+			formatting.clang_format.with({
+				extra_args = { "--fallback-style=Google" },
+			}),
+
+			-- PHP
+			diagnostics.phpcs.with({
+				extra_args = { "--standard=PSR12" },
+			}),
+			formatting.phpcbf.with({
+				extra_args = { "--standard=PSR12" },
+			}),
+
+			-- JS/TS
+			formatting.prettier,
+			diagnostics.eslint_d.with({
+				condition = function(utils)
+					-- only enable if root has .eslintrc.js or .eslintrc.cjs or .eslintrc.json
+					return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
+				end,
+			}),
+		}
+
 		null_ls.setup({
 			root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-			sources = {
-				formatting.clang_format,
-				formatting.stylua,
-				formatting.prettier,
-			},
+			sources = sources,
 			on_attach = function(current_client, bufnr)
 				if current_client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
